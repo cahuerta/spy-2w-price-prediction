6caadc58e63a2329c3f9c6998b1c3896ce35c343
cahuerta <cahuerta@gmail.com>
2026-02-09 22:38:47 -0300

    Update pipeline_router.py

--- pipeline_router.py
+++ pipeline_router.py
@@ -21,7 +21,7 @@ from decider import run_decider
 from model_runner import run_all_models
 from evaluator import evaluate_all
 
-from evaluador_cuantitativo_mercado import run_market_state
+from market_state_evaluator import run_market_state
 from market_qualitative_evaluator import evaluate_qualitative_market
 from market_orchestrator import MarketOrchestrator
 from trading_orchestrator import TradingOrchestrator
