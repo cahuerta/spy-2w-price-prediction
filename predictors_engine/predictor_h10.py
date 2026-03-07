6ff81075bfe9920032fb5241e11f215911e4dd22
cahuerta <cahuerta@gmail.com>
2026-03-07 14:48:16 -0300

    Update predictor_h10.py

--- predictors_engine/predictor_h10.py
+++ predictors_engine/predictor_h10.py
@@ -7,6 +7,7 @@ NO cambia la lógica matemática.
 import os
 import json
 import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import numpy as np
 import pandas as pd
 from datetime import datetime
