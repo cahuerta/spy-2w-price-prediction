51e8c408ca5c1bb3527d7756efedef4603db9c01
cahuerta <cahuerta@gmail.com>
2026-03-11 21:59:03 -0300

    Update evaluator.py

--- evaluator.py
+++ evaluator.py
@@ -310,17 +310,18 @@ def evaluate_all(
             try:
                 ev = fut.result()
                 if ev:
-                    ev_dict = asdict(ev)
-                    # cargar predicción original
-                    pred = load_json(f)
+                    
+                    ev = fut.result()
+                    if ev:
+                        ev_dict = asdict(ev)
+                        pred = load_json(f)
 
-                    if ev_dict.get("price_real") is not None:
-                        models_diag = evaluate_models(pred, ev_dict["price_real"])
+                        # MODELS PRIMERO
+                        models_diag = evaluate_models(pred, ev_dict.get("price_real"))
                         models_summary = summarize_models(models_diag)
 
                         ev_dict["models_diagnostics"] = models_diag
                         ev_dict["models_summary"] = models_summary
-
                     save_json(
                         eval_root / f.parent.name / f.name,
                         ev_dict,
